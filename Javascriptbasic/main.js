c74aabdbbc9f75c953350a718c168327c3e2614d
Thakur <raman_thakur4u@yahoo.co.in>
2015-08-23 17:05:00 +0530

    Javascript is dynamic language.
    Signed-off-by: Thakur <raman_thakur4u@yahoo.co.in>

---
+++ Javascriptbasic/main.js
@@ -0,0 +1,15 @@
+function Main(){
+    
+    var a="raman";
+    console.log(typeof a);
+    a=5;
+    console.log(typeof a);
+    a=function () {
+        
+    };
+    console.log(typeof a);
+    a={};
+    
+     console.log(typeof a);
+    
+}
