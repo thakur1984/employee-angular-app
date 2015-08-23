794d0834fc1686b99a2353a9a595b4e29b396316
Thakur <raman_thakur4u@yahoo.co.in>
2015-08-23 23:51:42 +0530

    Create Object with instance and prototype

--- Javascriptbasic/main.js
+++ Javascriptbasic/main.js
@@ -1,15 +1,49 @@
-function Main(){
+// var employee= {
+//     name:"Uday thakur",
+//     qualification:"Nursery",
+//     displayName:function(){
+//         console.log(this.name);
+//     }
+// }
+// employee.diplayQualification=function(){
+//     console.log(this.qualification);
+//     console.log(employee.qualification);
+// }
+// employee.diplayQualification();
+
+
+function Employee(name){
     
-    var a="raman";
-    console.log(typeof a);
-    a=5;
-    console.log(typeof a);
-    a=function () {
-        
-    };
-    console.log(typeof a);
-    a={};
+    //private variable
+    var panCard="abc",qualification="MCA";
+    //public variables or instanse
+    this.Name=name;
     
-     console.log(typeof a);
+    this.displayName=function(){
+        console.log("My name is: " + this.Name);
+    }
     
+    this.getQualification=function(){
+        console.log("My qualification is: "+qualification);
+    }
+    //public function access their private variable
+    this.getPancard=function(){
+        console.log("Pancard is: "+ panCard);
+        return panCard;
+    }
 }
+
+//var employee=Employee();
+var employee=new Employee("Raman");
+employee.displayName();
+//employee.getPancard();
+employee.getQualification();
+//attach prototype to object and  add public function
+Employee.prototype.get26AsForm=function(){
+    this.totalTax="1234";
+  var pancard=this.getPancard();
+   console.log("employee name:"+ this.Name  +"pan card number: "+ pancard +" do not have any 26as: "+ this.totalTax);
+}
+
+employee.get26AsForm();
+//console.log(employee.totalTax);
